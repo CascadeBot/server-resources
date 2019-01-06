83f4e0e6a7b14a02b00d07bd9ca198a7aa708f85
William Oldham <wegg7250@gmail.com>
2019-01-06 17:29:43 +0000

    Add header ;) And make script more error resistent, also jarservice!

--- cascade_initial_setup.sh
+++ cascade_initial_setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-#
-#
-#
-#
+###################################################################
+#                 Cascade Bot Server Setup Script                 #
+#                                                                 #
+#   Author: BinaryOverload (https://github.com/binaryoverload     #
+###################################################################
+
+
+# Stop script on any failure (Stops william screwing everything up :eyes:)
+set -e
+set -o pipefail
 
 ############### Constants ###############
 
@@ -41,6 +47,8 @@ if (( EUID != 0 )); then
 	exit 1
 fi
 
+test 1
+
 echo_green "Adding repositories!"
 for ppa in "${PPAS[@]}"
 do
@@ -67,3 +75,9 @@ echo debconf shared/accepted-oracle-license-v1-2 select true | debconf-set-selec
 echo debconf shared/accepted-oracle-license-v1-2 seen true | debconf-set-selections
 apt-get -y install oracle-java11-installer oracle-java11-set-default
 
+echo_green "Installing jarservice!"
+if [ ! -f jarservice ]; then
+	wget https://github.com/CascadeBot/server-resources/raw/master/jarservice
+fi
+cp jarservice /usr/bin/jarservice
+chmod 755 /usr/bin/jarservice
