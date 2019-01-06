d93fab38ea379fd676f28a8b3c198bd374a09c5b
William Oldham <wegg7250@gmail.com>
2019-01-06 21:35:44 +0000

    MongoDB

--- cascade_initial_setup.sh
+++ cascade_initial_setup.sh
@@ -81,3 +81,10 @@ if [ ! -f jarservice ]; then
 fi
 cp -v jarservice /usr/bin/jarservice
 chmod 755 /usr/bin/jarservice
+
+echo_green "Installing MongoDB!"
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+add-apt-repository "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse"
+apt-get update
+apt-get install -y mongodb-org
+
